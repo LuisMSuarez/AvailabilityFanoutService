136492a186276a1a67f3f15f4a7ed8dbd9ca3ce7
Luis M. Suarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-08-11 14:17:43 -0700

    Core code changes (#1)

---
+++ AvailabilityFanoutSvcApi/Controllers/PingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace AvailabilityFanoutSvcApi.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> logger;
+        private readonly HttpClient httpClient;
+        private readonly IEnumerable<string> urls =
+        [
+            "https://gamers-hub.azurewebsites.net/",
+            "https://gamers-hub-api.azurewebsites.net/v1/ping"
+        ];
+
+
+        public PingController(
+            ILogger<PingController> logger,
+            HttpClient httpClient)
+        {
+            this.logger = logger;
+            this.httpClient = httpClient;
+        }
+
+        [HttpGet(Name = "Ping")]
+        public async Task<IActionResult> Ping()
+        {
+            this.logger.LogInformation("Ping endpoint called");
+
+            var tasks = this.urls.Select(url => this.httpClient.GetAsync(url));
+            await Task.WhenAll(tasks);
+            return Ok();
+        }
+    }
+}
